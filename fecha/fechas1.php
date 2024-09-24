4e358c7f34c2b30ea9d841657088d2134d60568f
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-09-24 22:57:43 +0200

    fin fechas

--- fecha/fechas1.php
+++ fecha/fechas1.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Ejercico 1
+ * Ejercico 1 fecha y hora actual
  */
 
  $hoy= new DateTime('now');
  echo "la fecha y hora de hoy es:  ".$hoy->format("d-m-y H:i;s"),"<br>";
 
  /**
-  * Ejercicio 2
+  * Ejercicio 2 Mosrar zona horaria 
   */
 
   echo "nombre de la zona horaria por defecto:  ".date_default_timezone_get(),"<br>";
 
   /**
-   * Ejercicio 3
+   * Ejercicio 3 fecha dentro de 45 dias
    */
 
    $fecha = $hoy;
@@ -21,7 +21,7 @@
    echo "la fecha dentro de 45 dias es:  ".$fecha->format('Y-m-d'),"<br>";
 
    /**
-    * Ejercicio 4
+    * Ejercicio 4 numeros pasados desde el 1 de enero
     */
     $pasado= new DateTime('2024-01-01');
     $presente=$hoy;
@@ -29,7 +29,7 @@
     echo "dias pasados desde el dia 1 de enero:  ".$diferencia->format('%R%a dias de diferencia'),"<br>";
 
     /**
-     * Ejercicio 5
+     * Ejercicio 5 fecha y hora actuales de New York
      */
     $zona= new DateTimeZone('America/New_York');
     $horaNew_York= new DateTime('now',$zona);
@@ -37,7 +37,7 @@
     echo "fecha y hora actual de Nueva York:  ".$horaNew_York->format("d-m-y H:i;s"),"<br>";
 
     /**
-     * Ejercicio 6
+     * Ejercicio 6 Dia de la semana del dia 1 de enero 
      */
     $diaEnero = $pasado;
     echo "dia de la semana que era el dia 1 de enero:  ". $diaEnero->format("D");
