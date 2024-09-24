2b9098264a8d02203cae077a337cb7ad14b52875
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-09-24 22:39:10 +0200

    fin cadena 1

--- cadenas/cadenas1.php
+++ cadenas/cadenas1.php
@@ -7,33 +7,72 @@
 </head>
 <body>
     <?php
+    /**
+     * Ejercicio 1 Crea una variable 
+     */
     $nombre= "anaMArial";
+    /**
+     * Ejercicio 2 Muestralo por pantalla
+     */
     echo   $nombre . "\n";
     echo "<br>";
+    /**
+     * Ejercicio 3 Muestra la longitud
+     */
     echo strlen($nombre);
     echo "<br>";
+    /**
+     * Ejercicio 4 Pasarlo a mayuscula
+     */
     echo strtoupper($nombre);
     echo "<br>";
+    /**
+     * Ejercicio 5 Pasarlo a minuscula
+     */
     echo strtolower($nombre);
     echo "<br>";
+    /**
+     * Ejercicio 6 Mostrar numero veces aparece letra
+     */
     echo substr_count($nombre,"a");
     echo "<br>";
+    /**
+     * Ejercicio 7 Mostrar codigo ASCII primera letra
+     */
     $ascii = ord($nombre);
     print($ascii);
     echo "<br>";
+    /**
+     * Ejercicio 8 Muestra primera aparicion de a
+     */
     echo strpos($nombre,"A");
     echo "<br>";
+    /**
+     * Ejercicio 9 Lo mismo que la anterior pero con la ultima 
+     */
     echo strcspn($nombre,"a");
     echo "<br>";
+    /**
+     * Ejercicio 10 Sustituir letras por cero
+     */
     echo  str_ireplace('$nombre', '0', '$nombre');
     echo "<br>";
+    /**
+     * Ejercicio 11 indica si el nombre empieza por al
+     */
     if (str_ends_with($nombre,"al") ==true){
         echo " si ";
 
     }
     echo "<br>";
+    /**
+     * Ejercicio 12 elimina espacios al principio y final 
+     */
     echo rtrim($nombre);
     echo "<br>";
+    /**
+     * Ejercicio 13 Muestra variable primera letra mayuscula demas no 
+     */
 
     echo ucfirst(strtolower($nombre));
     echo "<br>";
