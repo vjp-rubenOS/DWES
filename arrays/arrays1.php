0175e868f56f07218a203aa09d682dc81e1e158f
Ruben Ortega Serradilla <rortegas04@iesvjp.es>
2024-09-24 20:33:49 +0200

    continuamos arrays1

--- arrays/arrays1.php
+++ arrays/arrays1.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * ejercicio 1
  */
@@ -59,14 +60,61 @@ shuffle($nombres);
    */
   $patron_buscar='[a]';
   $array_resultado = preg_grep($patron_buscar,$nombres);
-  print_r($array_resultado);
+  print_r($array_resultado)."<br>";
 
   /**
    * ejercicio 9
-   * pagina 1111
+   * 
    */
-  $alumnos[['Nombre'=>'Pepe','Edad'=>25,]
+  $alumnos=[
+    ['id'=>1,'Nombre'=>'Pepe','Edad'=>25],
+    ['id'=>2,'Nombre'=>'Juan','Edad'=>32],
+    ['id'=>3,'Nombre'=>'Maria','Edad'=>20]
   ];
+
+  /**
+   * ejercicio 10
+   */
+  
+  ?>
+  <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Alumnos</title>
+  
+</head>
+<body>
+    <h1>Lista de Alumnos</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Edad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($alumnos as $alumno): ?>
+                <tr>
+                    <td><?php echo $alumno['id']; ?></td>
+                    <td><?php echo $alumno['Nombre']; ?></td>
+                    <td><?php echo $alumno['Edad']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
+
+<?php
+/**
+ * Ejercicio 11
+ */
+
+?>  
+
 
 
 
