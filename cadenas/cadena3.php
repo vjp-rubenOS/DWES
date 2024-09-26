05f75d13ba6e8689896af058e607df56807568e7
Ruben Ortega Serradilla <rortegas04@iesvjp.es>
2024-09-26 18:45:24 +0200

    FIN

--- cadenas/cadena3.php
+++ cadenas/cadena3.php
@@ -9,13 +9,15 @@
 <body>
     <?php
     /**
-     * El protocolo utilizado y el nombre de usuario por ejemplo
+     * El protocolo utilizado 
      */
     echo "Utilizacion del protocolo"."<br>";
     $url = 'http://username:password@hostname:9090/path?arg=value#anchor';
-    print_r(parse_url($url))."<br>";
 
+    echo parse_url($url,PHP_URL_PASS)."<br>";
     echo parse_url($url,PHP_URL_USER)."<br>";
+    echo parse_url($url,PHP_URL_QUERY)."<br>";
+    echo parse_url($url,PHP_URL_SCHEME)."<br>";
 
     
     ?>
