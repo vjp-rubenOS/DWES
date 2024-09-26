7aef8c40b8e6df9ddfc6864ba227ea97aca2b87c
Ruben Ortega Serradilla <rortegas04@iesvjp.es>
2024-09-26 17:54:56 +0200

    fin cadena1

--- cadenas/cadenas1.php
+++ cadenas/cadenas1.php
@@ -13,53 +13,55 @@
     /**
      * Ejercicio 2 Muestralo por pantalla
      */
-    echo   $nombre . "\n";
+    echo   "El nombre es: ".$nombre . "\n";
     echo "<br>";
     /**
      * Ejercicio 3 Muestra la longitud
      */
-    echo strlen($nombre);
+    echo "La longitud del nombre es: ".strlen($nombre);
     echo "<br>";
     /**
      * Ejercicio 4 Pasarlo a mayuscula
      */
-    echo strtoupper($nombre);
+    echo "El nombre en mayusculas es: ".strtoupper($nombre);
     echo "<br>";
     /**
      * Ejercicio 5 Pasarlo a minuscula
      */
-    echo strtolower($nombre);
+    echo "El nombre en minuscula es: ".strtolower($nombre);
     echo "<br>";
     /**
      * Ejercicio 6 Mostrar numero veces aparece letra
      */
-    echo substr_count($nombre,"a");
+    echo "Numero de veces que aparece la 'a': ".substr_count($nombre,"a");
     echo "<br>";
     /**
      * Ejercicio 7 Mostrar codigo ASCII primera letra
      */
     $ascii = ord($nombre);
-    print($ascii);
+    print "El codigo ASCII de la primera letra es: ".($ascii);
     echo "<br>";
     /**
      * Ejercicio 8 Muestra primera aparicion de a
      */
-    echo strpos($nombre,"A");
+    echo "aparicion de la primera 'a' :".strpos($nombre,"A");
     echo "<br>";
     /**
      * Ejercicio 9 Lo mismo que la anterior pero con la ultima 
      */
-    echo strcspn($nombre,"a");
+    echo "aparicion de la ultima 'a' :".strcspn($nombre,"a");
     echo "<br>";
     /**
      * Ejercicio 10 Sustituir letras por cero
      */
-    echo  str_ireplace('$nombre', '0', '$nombre');
+    echo  "El nombre sustituido por '0' :".str_ireplace('$nombre', '0', '$nombre');
     echo "<br>";
     /**
      * Ejercicio 11 indica si el nombre empieza por al
      */
+    echo "¿El nombre empieza por al? "."<br>";
     if (str_ends_with($nombre,"al") ==true){
+        
         echo " si ";
 
     }
@@ -67,13 +69,13 @@
     /**
      * Ejercicio 12 elimina espacios al principio y final 
      */
-    echo rtrim($nombre);
+    echo "Nombre con el espacio eliminado al principio y al final : ".rtrim($nombre);
     echo "<br>";
     /**
      * Ejercicio 13 Muestra variable primera letra mayuscula demas no 
      */
 
-    echo ucfirst(strtolower($nombre));
+    echo "nombre con la primera letra en mayuscula: ".ucfirst(strtolower($nombre));
     echo "<br>";
 
 
