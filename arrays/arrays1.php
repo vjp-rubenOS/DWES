afec25d0b286720906f76e109bfb31acbd24fab7
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-09-29 10:54:52 +0200

    fin ejercicio array

--- arrays/arrays1.php
+++ arrays/arrays1.php
@@ -5,102 +5,103 @@
     <meta name="autor" content="Ruben Ortega Serradilla">
     <title>arrays 1</title>
 </head>
+
 <body>
-    
-
-<?php
 
 
-/**
- * ejercicio 1 Crear un array
- */
-$nombres = array('Ruben','Pepe', 'Juan', 'Maria');
-echo "Componentes del array : ";
-print_r ($nombres)."hola";
-echo "<br>";
+    <?php
 
-/**
- * ejercicio 2 Mostrar numero de elementos
- */
-echo "El numero de elementos es: ".count($nombres);
-echo "<br>";
 
-/**
- * ejercicio 3 Crear una cadena con los alumnos y el array separado por espacio
- */
+    /**
+     * ejercicio 1 Crear un array
+     */
+    $nombres = array('Ruben', 'Pepe', 'Juan', 'Maria');
+    echo "Componentes del array : ";
+    print_r($nombres) . "hola";
+    echo "<br>";
 
-$cadena = implode(' y ', $nombres);
-echo "Los integrantes de clase son: $cadena ";
-echo "<br>";
+    /**
+     * ejercicio 2 Mostrar numero de elementos
+     */
+    echo "El numero de elementos es: " . count($nombres);
+    echo "<br>";
 
-/**
- * ejercicio 4 Mostrar ordenado 
- */
+    /**
+     * ejercicio 3 Crear una cadena con los alumnos y el array separado por espacio
+     */
 
-sort($nombres);
-echo "Array con los nombres ordenados: ";
-print_r($nombres);
-echo "<br>";
+    $cadena = implode(' y ', $nombres);
+    echo "Los integrantes de clase son: $cadena ";
+    echo "<br>";
 
-/**
- * ejercicio 5 Mostrar array orden inverso
- */
-// primero los descoloco con shuffle
+    /**
+     * ejercicio 4 Mostrar ordenado 
+     */
 
-shuffle($nombres);
+    sort($nombres);
+    echo "Array con los nombres ordenados: ";
+    print_r($nombres);
+    echo "<br>";
 
-// despues lo ponemos en orden inverso
- rsort($nombres);
- echo "Array en orden inverso: ";
- print_r($nombres);
- echo "<br>";
+    /**
+     * ejercicio 5 Mostrar array orden inverso
+     */
+    // primero los descoloco con shuffle
 
- /**
-  * ejercicio 6 Mostrar posicion de mi nombre
-  */
-  $buscar_posicion = array_search('Ruben',$nombres);
-  echo "Mi nombre esta en la posicion: $buscar_posicion";
-  echo "<br>";
+    shuffle($nombres);
 
-  /**
-   * ejercicio 7 Mostrar array en orden aleatorio
-   */
-  shuffle($nombres);
-  echo "Array con el orden aleatorio: ";
-  print_r($nombres) ;
-  echo "<br>";
-  /**
-   * ejercicio 8 Mostrar alumnos que contangan la a 
-   */
-  $patron_buscar='[a]';
-  $array_resultado = preg_grep($patron_buscar,$nombres);
-  echo "Nombre de alumnos que tengan la a";
-  print_r($array_resultado)."<br>";
+    // despues lo ponemos en orden inverso
+    rsort($nombres);
+    echo "Array en orden inverso: ";
+    print_r($nombres);
+    echo "<br>";
 
-  /**
-   * ejercicio 9 crear un array compuesto de arrays 
-   * 
-   */
-  $alumnos=[
-    ['id'=>1,'Nombre'=>'Pepe','Edad'=>25],
-    ['id'=>2,'Nombre'=>'Juan','Edad'=>32],
-    ['id'=>3,'Nombre'=>'Maria','Edad'=>20]
-  ];
+    /**
+     * ejercicio 6 Mostrar posicion de mi nombre
+     */
+    $buscar_posicion = array_search('Ruben', $nombres);
+    echo "Mi nombre esta en la posicion: $buscar_posicion";
+    echo "<br>";
 
-  /**
-   * ejercicio 10 crear una tabla con los datos
-   */
-  echo '<br>';
-  echo '<br>';
-  echo "Tabla con los datos del array";
- 
-   echo '<table border="1">';
+    /**
+     * ejercicio 7 Mostrar array en orden aleatorio
+     */
+    shuffle($nombres);
+    echo "Array con el orden aleatorio: ";
+    print_r($nombres);
+    echo "<br>";
+    /**
+     * ejercicio 8 Mostrar alumnos que contangan la a 
+     */
+    $patron_buscar = '[a]';
+    $array_resultado = preg_grep($patron_buscar, $nombres);
+    echo "Nombre de alumnos que tengan la a";
+    print_r($array_resultado) . "<br>";
+
+    /**
+     * ejercicio 9 crear un array compuesto de arrays 
+     * 
+     */
+    $alumnos = [
+        ['id' => 1, 'Nombre' => 'Pepe', 'Edad' => 25],
+        ['id' => 2, 'Nombre' => 'Juan', 'Edad' => 32],
+        ['id' => 3, 'Nombre' => 'Maria', 'Edad' => 20]
+    ];
+
+    /**
+     * ejercicio 10 crear una tabla con los datos
+     */
+    echo '<br>';
+    echo '<br>';
+    echo "Tabla con los datos del array";
+
+    echo '<table border="1">';
     echo '<tr>';
     echo '<th>Id</th>';
     echo '<th>Nombre</th>';
     echo '<th>Edad</th>';
     echo '</tr>';
-    foreach($alumnos as $alumno){
+    foreach ($alumnos as $alumno) {
         echo '<tr>';
         echo '<td>' . $alumno['id'] . '</td>';
         echo '<td>' . $alumno['Nombre'] . '</td>';
@@ -109,39 +110,30 @@ shuffle($nombres);
     }
     echo '</table>' . '<br>';
 
-  
-
-/**
- * Ejercicio 11 conseguir array indexsado
- */
 
 
-
-$soloNombres= array_column($alumnos,'Nombre');
-echo "Array con solo los nombres: ";
-print_r($soloNombres);
-echo "<br>";
-
-/**
- * Ejercicio 12 Crar array con numeros y usando funcion suma
- */
-
- $numeros = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-
-$suma = array_sum($numeros);
-
-
-echo "La suma de los 10 numeros es: " . $suma."<br>";
-
-?> 
-</body> 
+    /**
+     * Ejercicio 11 conseguir array indexsado
+     */
 
 
 
+    $soloNombres = array_column($alumnos, 'Nombre');
+    echo "Array con solo los nombres: ";
+    print_r($soloNombres);
+    echo "<br>";
+
+    /**
+     * Ejercicio 12 Crar array con numeros y usando funcion suma
+     */
+
+    $numeros = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 
+    $suma = array_sum($numeros);
 
 
+    echo "La suma de los 10 numeros es: " . $suma . "<br>";
 
- 
+    ?>
+</body>
---
+++ arrays/arrays2.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="autor" content="Ruben Ortega Serradilla">
+    <title>Cadena 2</title>
+    <style>
+        /* Estilo para la tabla */
+        table {
+            border-collapse: collapse;
+            width: 60%;
+            margin: 20px auto;
+            background-color: #DCDCDC;
+            
+        }
+        
+        /* Este se encarga de las celdas */
+        td, th {
+            border: 3px solid white;
+            padding: 8px;
+            
+        }
+        
+        /* Color de fondo para las filas impares */
+        tr:nth-child(2n+1) {
+            background-color: #f2f2f3;
+            
+        }
+        
+       
+        /* Color de la primera fila  */
+        tr:nth-child(1) {
+            background-color: blue;
+            color: white;
+            font-size: large;
+        }
+        
+       
+    </style>
+</head>
+
+<body>
+    <?php
+    /**
+     * arrat multidimensional
+     */
+    $paises = [
+        ['Pais' => 'Alemania', 'Capital' => 'Berlin', 'Extension' => 557046, 'Habitantes' => 78420000],
+        ['Pais' => 'Austria', 'Capital' => 'Viena', 'Extension' => 83849, 'Habitantes' => 7614000],
+        ['Pais' => 'Belgica', 'Capital' => 'Bruselas', 'Extension' => 30518, 'Habitantes' => 9932000]
+
+
+    ];
+    
+        
+    
+    ?>
+    
+    <table  > 
+        <tr >
+            <?php
+            /*
+            creamos una tabla y dentro un foreach para sacar el valor de las claves para la cabecera 
+            */
+            foreach(array_keys($paises[0]) as $clave){
+                echo "<td>".$clave."</td>";
+                
+            }
+            ?>
+        </tr>
+        <?php
+        /*
+        creamos dos foreach uno para sacar las filas y el otro para los datos que van dentro de la fila 
+        */
+        foreach($paises as $pais){
+            echo "<tr >";
+            foreach($pais as $valor){
+                echo "<td>".$valor."</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    
+</body>
