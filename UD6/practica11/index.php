a5031c8740786b0562e631385c67121bf407aa55
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-27 09:23:38 +0100

    fin practica 11

--- UD6/practica11/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//Llamamiento a cada una de las partes de la practica
-require_once 'entiti/SelectorIndividual.class.php';
-require_once 'entiti/SIRadioOpcion.php';
-require_once 'entiti/SISelect.php';
-require_once 'view/index.view.php'; 
-
-?>
