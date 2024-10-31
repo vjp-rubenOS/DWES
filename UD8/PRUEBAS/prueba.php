37cbf1bba99300536d4d744ed1e26c9fdc8473a0
2DAW-Ruben Ortega <rortegas04@iesvjp.es>
2024-10-31 19:23:13 +0100

    prueba

---
+++ UD8/PRUEBAS/prueba.php
@@ -0,0 +1,4 @@
+<?php 
+$pdo = new PDO(`mysql:host-localhost;dbname=proyecto;charset='utf8'`,'user','user');
+//$pdo->exec("INSERT INTO asociados(nombre,logo,descripcion" VALUES ('Peter','asd'))
+?>
